5712573119df6094ebd743cb6b9ed5518971b2c0
gba-3 <gbayru3@gmail.com>
2022-06-11 10:48:38 +0900

    jwtトークンの検証を追加

--- middleware/auth.go
+++ middleware/auth.go
@@ -12,16 +12,17 @@ func WithToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("Authorization")
 		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
-		// _, err := verifyToken(tokenString)
-		// if err != nil {
-		// 	http.Error(w, err.Error(), http.StatusBadRequest)
-		// }
+		_, err := verifyToken(tokenString)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
 
 		ctx := context.WithValue(r.Context(), "token", tokenString)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
+// JWT Token 検証
 func verifyToken(tokenString string) (*jwt.Token, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return []byte("secret"), nil

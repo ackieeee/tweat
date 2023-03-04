82960e085026fabdb370c8000c6ec7187a4468ff
gba-3 <gbayru3@gmail.com>
2023-03-05 00:08:10 +0900

    server関数 実装

--- app/main.go
+++ app/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -9,6 +14,7 @@ import (
 	"github.com/sugartr3e/tweat/handler"
 	mw "github.com/sugartr3e/tweat/middleware"
 	"github.com/sugartr3e/tweat/registry"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 func init() {
@@ -19,11 +25,12 @@ func init() {
 	time.Local = jst
 }
 
-func main() {
+func Server(ctx context.Context) error {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	ah := registry.NewRegistry()
+	// ルーティング
 	r.Route("/tweats", func(r chi.Router) {
 		r.Use(mw.WithToken)
 		r.Get("/", handler.JsonHandler(ah.Th.GetAll).ServeHTTP)
@@ -39,5 +46,38 @@ func main() {
 	r.Route("/login", func(r chi.Router) {
 		r.Post("/", handler.JsonHandler(ah.Uh.Login).ServeHTTP)
 	})
-	http.ListenAndServe(":3030", r)
+
+	// サーバー起動
+	srv := &http.Server{
+		Handler: otelhttp.NewHandler(r, "",
+			otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
+				return r.Method + ": " + r.URL.Path
+			}),
+			otelhttp.WithMessageEvents(
+				otelhttp.ReadEvents,
+				otelhttp.WriteEvents,
+			)),
+		Addr: "0.0.0.0:3030",
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Server closed with error: %s\n", err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+	log.Printf("SIGNAL %d received, then shutdown\n", <-quit)
+
+	// タイムアウト処理
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	return srv.Shutdown(ctx)
+}
+
+func main() {
+	ctx := context.Background()
+	if err := Server(ctx); err != nil {
+		log.Fatal(err.Error())
+	}
 }
